bcc5b530e5a59a903295875a884088b80aea214b
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-11-17 20:44:36 -0500

    Finished

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledFriend = styled.div`
+  width: 60%;
+  display:flex;
+  justify-content: space-between;
+  padding: 8px;
+  border-bottom: 2px solid white;
+
+  background-color: ${pr => pr.theme.primaryColor};
+  color: ${pr => pr.theme.black};
+
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: ${pr => pr.theme.secondaryColor};
+  }
+`
 
 export default function Friend({ info, action }) {
   return (
-    <div className='friend'>
+    <StyledFriend className='friend'>
       {info.name}
       <button onClick={() => action(info.id)}>
         See details
       </button>
-    </div>
+    </StyledFriend>
   )
 }
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,1 +1,9 @@
 // theme object here
+export default {
+	primaryColor: 'Aqua',
+	secondaryColor: 'aquamarine',
+	tertiaryColor: 'mediumseagreen',
+	black: 'Gray',
+	white: 'white',
+	breakpointMobile: '(max-width: 550px)',
+};
